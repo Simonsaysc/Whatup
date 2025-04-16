6081fe06ca9b91b809b6fa9c13e8a8a3e214d196
Simonsaysc <salonso@miuandes.cl>
2025-04-16 16:33:03 -0400

    Database views

---
+++ app/controllers/chats_controller.rb
@@ -0,0 +1,8 @@
+class ChatsController < ApplicationController
+  def index
+    @chats = Chat.all
+  end
+  def show
+    @chat = Chat.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  root "pages#home"
+  resources :users
+  resources :messages
+  resources :chats
 end
